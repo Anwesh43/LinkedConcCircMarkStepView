b91d62e9198d5e076a089e51ca1438403d1dfb5c
Anwesh43 <anweshthecool0@gmail.com>
2018-12-05 10:36:51 +0530

    adding libraries and defining constants

--- conccircmarkview/src/main/java/com/anwesh/uiprojects/conccircmarkview/ConcCircMarkStepView.kt
+++ conccircmarkview/src/main/java/com/anwesh/uiprojects/conccircmarkview/ConcCircMarkStepView.kt
@@ -3,5 +3,20 @@ package com.anwesh.uiprojects.conccircmarkview
 /**
  * Created by anweshmishra on 05/12/18.
  */
-class ConcCircMarkStepView {
-}
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 6
+val sizeFactor : Int = 3
+val strokeFactor : Int = 90
+val scDiv : Double = 0.51
+val scGap : Float = 0.05f
+val color : Int = Color.parseColor("#01579B")
