419c7e6c2ff958a0ab3ebe6be3eeb166eefa1a0a
Anwesh43 <anweshthecool0@gmail.com>
2018-12-05 11:03:26 +0530

    adding ConcCircMoveStep class

--- conccircmarkview/src/main/java/com/anwesh/uiprojects/conccircmarkview/ConcCircMarkStepView.kt
+++ conccircmarkview/src/main/java/com/anwesh/uiprojects/conccircmarkview/ConcCircMarkStepView.kt
@@ -162,7 +162,30 @@ class ConcCircMarkStepView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class ConcCircMarkStep(var i : Int) {
+        private val root : CCMSNode = CCMSNode(0)
+        private var curr : CCMSNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
