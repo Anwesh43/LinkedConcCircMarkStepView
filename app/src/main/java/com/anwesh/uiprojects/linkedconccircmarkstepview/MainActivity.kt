d340e4a36f7a064a726b887ed106068fc5b4bd9f
Anwesh43 <anweshthecool0@gmail.com>
2018-12-05 11:17:53 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedconccircmarkstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedconccircmarkstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedconccircmarkstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.conccircmarkview.ConcCircMarkStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ConcCircMarkStepView.create(this)
     }
 }
